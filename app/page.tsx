83c8b3ed845e1d392bab0b58c73cf19bc3ce99a8
nekodayo2222 <99164438+nekodayo2222@users.noreply.github.com>
2024-07-21 13:24:50 +0900

    Update page.tsx to use Next.js Link component for better navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   const content = [
     {
@@ -26,7 +28,7 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:grid-cols-4 lg:text-left">
         {content.map((data) => (
           <>
-            <a
+            <Link
               href={data.href}
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
@@ -39,7 +41,7 @@ export default function Home() {
               <p className="m-0 max-w-[30ch] text-sm opacity-50">
                 {data.description}
               </p>
-            </a>
+            </Link>
           </>
         ))}
       </div>
